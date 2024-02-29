5485619b5180ed9f71b75fa2c3f11a6cb6169c5b
Terry Petersen <arigorn15@gmail.com>
2024-02-29 17:07:38 -0500

    Add database seeding to repository

---
+++ src/db/data/seeds/b_address.js
@@ -0,0 +1,8 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  // Deletes ALL existing entries
+  await knex.schema.raw('TRUNCATE shopping.address RESTART IDENTITY CASCADE');
+};
