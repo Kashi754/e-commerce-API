12d9cf82b64b79035e29180d1dd4564967b4e370
Terry Petersen <arigorn15@gmail.com>
2024-03-03 13:29:36 -0500

    Add console.log for debugging shipping route

--- src/middleware/verifyUserLoggedIn.js
+++ src/middleware/verifyUserLoggedIn.js
@@ -1,5 +1,6 @@
 const verifyUserLoggedIn = (req, res, next) => {
   if (!req.user) {
+    console.log(req);
     const error = new Error('Please log in!');
     error.status = 401;
     return next(error);
